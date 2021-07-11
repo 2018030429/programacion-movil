a25b1834ac150c46f9dfacd161e152b8668f990f
Enrique Contreras <2018030429@upsin.edu.mx>
2021-07-10 19:26:33 -0600

    ActivityMain modified

--- Baby/app/src/main/java/com/upsin/baby/MainActivity.java
+++ Baby/app/src/main/java/com/upsin/baby/MainActivity.java
@@ -3,46 +3,72 @@ package com.upsin.baby;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.SearchView;
-import android.widget.Toast;
-import java.util.ArrayList;
-import java.util.Arrays;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
   private RecyclerView _recyclerView;
   private RecyclerView.LayoutManager _layoutManager;
+  private FloatingActionButton _btnNewProduct;
+  private MyApplication app;
+
+  private Item _holderItem = null;
+  private int _position = -1;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    MyApplication app = (MyApplication) getApplication();
+    this.initializeComponents();
+
+    this._btnNewProduct.setOnClickListener(v -> {
+      this._holderItem = null;
+      this.createActivityForResult(0);
+    });
+
+    app.get_adapter().setOnClickListener(v -> {
+      int position = this._recyclerView.getChildAdapterPosition(v);
+      this._holderItem = app.get_itemList().get(position);
+      this.createActivityForResult(1);
+    });
+  }
+
+  private void createActivityForResult(int requestCode) {
+    Intent intent = new Intent(MainActivity.this, RegistrationActivity.class);
+    Bundle bundleObject = new Bundle();
+
+    bundleObject.putSerializable("item", this._holderItem);
+    bundleObject.putInt("position", this._position);
+    intent.putExtras(bundleObject);
+
+    startActivityForResult(intent, requestCode);
+  }
+
+  private void initializeComponents() {
+    this.app = (MyApplication) getApplication();
     this._recyclerView = findViewById(R.id.recyclerId);
     this._recyclerView.setAdapter(app.get_adapter());
 
     this._layoutManager = new LinearLayoutManager(this);
     this._recyclerView.setLayoutManager(this._layoutManager);
 
-    app.get_adapter().setOnClickListener(v -> {
-      int position = this._recyclerView.getChildAdapterPosition(v);
-      String displayItem = app.get_itemList().get(position).getHeader();
+    this._btnNewProduct = findViewById(R.id.btnNewProduct);
+  }
 
-      Toast.makeText(
-        MainActivity.this,
-        "You Clicked: " + displayItem,
-        Toast.LENGTH_SHORT
-      ).show();
-    });
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+
+    Objects.requireNonNull(this._recyclerView.getAdapter()).notifyDataSetChanged();
+    this._position = -1;
   }
 
   @Override
