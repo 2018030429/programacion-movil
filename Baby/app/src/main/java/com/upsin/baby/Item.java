144f5f9679a38ddbe21449fc6e7e1f883d315cc8
Enrique Contreras <2018030429@upsin.edu.mx>
2021-07-10 18:27:55 -0600

    Item modified

--- Baby/app/src/main/java/com/upsin/baby/Item.java
+++ Baby/app/src/main/java/com/upsin/baby/Item.java
@@ -1,23 +1,22 @@
 package com.upsin.baby;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.net.Uri;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Item {
 
-  private final int _image;
-  private final String _header;
-  private final String _description;
+  private String _image;
+  private String _header;
+  private String _description;
 
-  public Item(int image, String header, String description) {
+  public Item(String image, String header, String description) {
     this._image = image;
     this._header = header;
     this._description = description;
   }
 
-  public int getImage() {
+  public String getImage() {
     return _image;
   }
 
@@ -29,34 +28,120 @@ public class Item {
     return _header;
   }
 
+  public void setImage(String _image) {
+    this._image = _image;
+  }
+
+  public void setDescription(String _description) {
+    this._description = _description;
+  }
+
+  public void setHeader(String _header) {
+    this._header = _header;
+  }
+
   public static ArrayList<Item> getAllItems() {
     ArrayList<Item> itemList = new ArrayList<>();
 
-    itemList.add(new Item(R.drawable.babyitem_1, "Capa", "Capa azul marino"));
-    itemList.add(new Item(R.drawable.babyitem_2, "Babero", "Babero Super Bowl"));
-    itemList.add(new Item(R.drawable.babyitem_3, "Pijama", "Pijama de dinosaurios blanca"));
-    itemList.add(new Item(R.drawable.babyitem_4, "Pijama", "Pijama de dinosaurios azul"));
-    itemList.add(new Item(R.drawable.babyitem_5, "Pijama", "Pijama 4 de julio"));
-    itemList.add(new Item(R.drawable.babyitem_6, "Pijama", "Pijama de gatitos"));
-    itemList.add(new Item(R.drawable.babyitem_7, "Pijama", "Pijama 4 de julio"));
-    itemList.add(new Item(R.drawable.babyitem_8, "Calcetas", "Calcetas de colores"));
-    itemList.add(new Item(R.drawable.babyitem_9, "Traje", "Traje de vaquero"));
-    itemList.add(new Item(R.drawable.babyitem_10, "Joggers", "Joggers azules"));
-    itemList.add(new Item(R.drawable.babyitem_11, "Gorro", "Gorro blanco y gris"));
-    itemList.add(new Item(R.drawable.babyitem_12, "Mameluco", "Mameluco Pride"));
-    itemList.add(new Item(R.drawable.babyitem_13, "Pijama", "Pijama Halloween"));
-    itemList.add(new Item(R.drawable.babyitem_14, "Gorro", "Gorro de osito"));
-    itemList.add(new Item(R.drawable.babyitem_15, "Mameluco", "Mameluco Super Bowl"));
-    itemList.add(new Item(R.drawable.babyitem_16, "Traje", "Traje NYC"));
-    itemList.add(new Item(R.drawable.babyitem_17, "Traje", "Traje MLB"));
-    itemList.add(new Item(R.drawable.babyitem_18, "Traje", "Traje de cumpleaños"));
-    itemList.add(new Item(R.drawable.babyitem_19, "Sandalias", "Sandalias azules"));
-    itemList.add(new Item(R.drawable.babyitem_20, "Tennis", "Tennis celestes"));
-    itemList.add(new Item(R.drawable.babyitem_21, "Lentes", "Lentes de sol color naranja"));
-    itemList.add(new Item(R.drawable.babyitem_22, "Sandalias", "Sandalias Vintage"));
-    itemList.add(new Item(R.drawable.babyitem_23, "Chanclas", "Chanclas Pride"));
-    itemList.add(new Item(R.drawable.babyitem_24, "Mameluco", "Mameluco Howard"));
-    itemList.add(new Item(R.drawable.babyitem_25, "Mameluco", "Mameluco temático"));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_1).toString(),
+      "Capa", "Capa azul marino"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_2).toString(),
+      "Babero", "Babero Super Bowl"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_3).toString(),
+      "Pijama", "Pijama de dinosaurios blanca"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_4).toString(),
+      "Pijama", "Pijama de dinosaurios azul"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_5).toString(),
+      "Pijama", "Pijama 4 de julio"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_6).toString(),
+      "Pijama", "Pijama de gatitos"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_7).toString(),
+      "Pijama", "Pijama 4 de julio"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_8).toString(),
+      "Calcetas", "Calcetas de colores"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_9).toString(),
+      "Traje", "Traje de vaquero"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_10).toString(),
+      "Joggers", "Joggers azules"  ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_11).toString(),
+      "Gorro", "Gorro blanco y gris"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_12).toString(),
+      "Mameluco", "Mameluco Pride"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_13).toString(),
+      "Pijama", "Pijama Halloween"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_14).toString(),
+      "Gorro", "Gorro de osito"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_15).toString(),
+      "Mameluco", "Mameluco Super Bowl"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_16).toString(),
+      "Traje", "Traje NYC"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_17).toString(),
+      "Traje", "Traje MLB"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_18).toString(),
+      "Traje", "Traje de cumpleaños"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_19).toString(),
+      "Sandalias", "Sandalias azules"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_20).toString(),
+      "Tennis", "Tennis celestes"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_21).toString(),
+      "Lentes", "Lentes de sol color naranja"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_22).toString(),
+      "Sandalias", "Sandalias Vintage"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_23).toString(),
+      "Chanclas", "Chanclas Pride"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_24).toString(),
+      "Mameluco", "Mameluco Howard"
+    ));
+    itemList.add(new Item(
+      Uri.parse("android.resource://com.upsin.baby/"+R.drawable.babyitem_25).toString(),
+      "Mameluco", "Mameluco temático"
+    ));
 
     return itemList;
   }
