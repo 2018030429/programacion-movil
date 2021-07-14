2e6e2b81ebd1a216fefcd594eda376c7ff2a6cbc
Enrique Contreras <2018030429@upsin.edu.mx>
2021-07-14 15:17:00 -0600

    FIX Table definition

--- CustomAdapterDemo/app/src/main/java/com/upsin/customadapterdemo/modelo/StudentDBHelper.java
+++ CustomAdapterDemo/app/src/main/java/com/upsin/customadapterdemo/modelo/StudentDBHelper.java
@@ -8,14 +8,14 @@ public class StudentDBHelper extends SQLiteOpenHelper {
 
   private static final String TEXT_TYPE = " TEXT";
   private static final String INTEGER_TYPE = " INTEGER";
-  private static final String COMMA_SEP = " ,";
+  private static final String COMMA_SEP = ", ";
   private static final String SQL_CREATE_STUDENT =
     "CREATE TABLE " + TableDefine.Students.TABLE_NAME +
       " (" + TableDefine.Students.COLUMN_NAME_ID + " INTEGER PRIMARY KEY, " +
         TableDefine.Students.COLUMN_NAME_ENROLLMENT + TEXT_TYPE + COMMA_SEP +
         TableDefine.Students.COLUMN_NAME_NAME       + TEXT_TYPE + COMMA_SEP +
         TableDefine.Students.COLUMN_NAME_CAREER     + TEXT_TYPE + COMMA_SEP +
-        TableDefine.Students.COLUMN_NAME_PHOTO      + INTEGER_TYPE + COMMA_SEP +
+        TableDefine.Students.COLUMN_NAME_PHOTO      + INTEGER_TYPE +
       ") ";
   private static final String SQL_DELETE_STUDENT =
     "DROP TABLE IF EXISTS " + TableDefine.Students.TABLE_NAME;
